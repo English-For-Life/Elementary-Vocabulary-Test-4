42cd8368dd8ad97f1099ef6693fa7beeedea377a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-26 19:38:48 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"I must buy things ______ my house.",
+        options:["for", "of", "with", "in"],
+        answer: 0
+    },
+    {
+        q:"Can I try this ______ on?",
+        options:["bottle", "jacket", "paper", "shoes"],
+        answer: 1
+    },
+    {
+        q:"Can I have my ______ back?",
+        options:["pay", "cost", "money", "cash"],
+        answer: 2
+    },
+    {
+        q:"I took some biscuits from the ______ in the supermarket.",
+        options:["bottle", "tin", "bin", "shelf"],
+        answer: 3
+    },
+    {
+        q:"We give the money to the ______ in the shop.",
+        options:["cashier", "customer", "manager", "queue"],
+        answer: 0
+    },
+    {
+        q:"What is the money you get, usually weekly or hourly?",
+        options:["salary", "wages", "cash", "pay"],
+        answer: 1
+    },
+    {
+        q:"We call abilities you can do as ______ .",
+        options:["job", "quality", "skills", "experience"],
+        answer: 2
+    },
+    {
+        q:"Work of the same type you have done before is called ______ .",
+        options:["job", "work", "duty", "experience"],
+        answer: 3
+    },
+    {
+        q:"Certificates and exams passed mean ______ ?",
+        options:["qualifications", "skills", "experience", "job"],
+        answer: 0
+    },
+    {
+        q:"What is a talk with a company about a possible job?",
+        options:["speaking", "interview", "experience", "talk"],
+        answer: 1
+    }
+
+
+
+]
